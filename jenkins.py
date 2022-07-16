2b66f60c940c548707adad884cdfd75baeda9c72
Sangrambadi Sahoo <49452509+sahoo1991@users.noreply.github.com>
2022-07-16 17:22:36 +0530

    Update jenkins.py
    msg added

--- jenkins.py
+++ jenkins.py
@@ -1,8 +1,1 @@
-import argparse
-
-if __name__ == "__main__":
-    print("My name is sangram")
-    args = argparse.ArgumentParser(description='Process some integers.')
-    args.add_argument('--name', default="Sangram", help="Enter the name")
-    arg = args.parse_args()
-    print(arg.name)
+print("This is the first out put message that i get from jenkins")
