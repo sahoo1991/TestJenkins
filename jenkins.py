38430610418fac2ca9df5055a9ff8247a8fa85a4
Sangrambadi Sahoo <49452509+sahoo1991@users.noreply.github.com>
2022-07-16 17:09:09 +0530

    Create jenkins.py
    first line

---
+++ jenkins.py
@@ -0,0 +1,8 @@
+import argparse
+
+if __name__ == "__main__":
+    print("My name is sangram")
+    args = argparse.ArgumentParser(description='Process some integers.')
+    args.add_argument('--name', default="Sangram", help="Enter the name")
+    arg = args.parse_args()
+    print(arg.name)
